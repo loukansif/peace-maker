c3a8f2c8e829792eeea4ad983fa7572a5a0286b8
loukansif <shakbaz@gmail.com>
2022-07-19 10:48:23 +0200

    Display haiku userConnected

--- client/src/components/TabsProfil.jsx
+++ client/src/components/TabsProfil.jsx
@@ -1,81 +1,104 @@
 import React, { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import Paper from '@mui/material/Paper';
-import Avatar from '@mui/material/Avatar';
-
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Paper from "@mui/material/Paper";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
 
 export default function TabsProfil() {
-    const [value, setValue] = useState('1');
-    const [haikus, setHaikus] = useState([]);
-    const [users, setUsers] = useState([]);
+  const [value, setValue] = useState("1");
+  const [haikus, setHaikus] = useState([]);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    // récupération de tous mes Haikus
+  // récupération de tous mes Haikus
 
-    const getHaikus = () => {
-        fetch("http://localhost:5000/haikus")
-            .then((response) => {
-                return response.json();
-            })
-            .then((result) => {
-                setHaikus(result);
-            })
-            .catch((error) => console.log(error));
-    };
+  const getHaikus = () => {
+    fetch("http://localhost:5000/haikus")
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setHaikus(result);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    const getUsers = () => {
-        fetch("http://localhost:5000/users")
-            .then((response) => {
-                return response.json();
-            })
-            .then((result) => {
-                setUsers(result);
-            })
-            .catch((error) => console.log(error));
-    };
-
-    useEffect(() => {
-        getUsers();
-        getHaikus();
-    }, []);
-    return (
-
-        <Box sx={{ dp: 2, width: '80%', typography: 'body1' }} className="margTop">
-            <TabContext value={value}>
-                <Box sx={{ borderBottom: 0, borderColor: 'divider' }} display='flex' justifyContent='center'>
-                    <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        <Tab label="Haïkus" value="1" sx={{ color: "whitesmoke !important" }} />
-                        <Tab label="Favoris" value="2" sx={{ color: "whitesmoke !important" }} />
-                    </TabList>
-                </Box>
-                <TabPanel value="1">
-                    <div className="haikus">
-                        {haikus.map((haiku) => {
-                            return(
-                            (localStorage.getItem('userId') === haiku.userId ?
-                                <Paper elevation={8} sx={{ padding: 2, backgroundColor: 'rgba(255,255,255,0)', color: 'whitesmoke', width: '90%' }}>
-                                    <Avatar className="totemPosition" sx={{ width: 70, height: 70 }} src={localStorage.getItem('userTotem')} />
-                                    <p>{haiku.line1}</p>
-                                    <p>{haiku.line2}</p>
-                                    <p>{haiku.line3}</p>
-                                    <Avatar sx={{ width: 30, height: 30, position: 'relative', left: '100%', }} src={haiku.emoji} />
-                                </Paper>
-                                : null))
-                        }
-                        )}
-
-                    </div>
-                </TabPanel>
-                <TabPanel value="2">My favoris</TabPanel>
-            </TabContext>
+  useEffect(() => {
+    getHaikus();
+  }, []);
+  return (
+    <Box sx={{ dp: 2, width: "100%", typography: "body1" }} className="margTop">
+      <TabContext value={value}>
+        <Box
+          sx={{ borderBottom: 0, borderColor: "divider" }}
+          display="flex"
+          justifyContent="center"
+        >
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab
+              label="Haïkus"
+              value="1"
+              sx={{ color: "whitesmoke !important" }}
+            />
+            <Tab
+              label="Favoris"
+              value="2"
+              sx={{ color: "whitesmoke !important" }}
+            />
+          </TabList>
         </Box>
 
-    );
+        <TabPanel value="1">
+          <div className="haikus">
+            {haikus.map((haiku) => {
+              return localStorage.getItem("userId") === haiku.user._id ? (
+                <>
+                  <Paper
+                    key={haiku._id}
+                    elevation={8}
+                    sx={{
+                      padding: 2,
+                      backgroundColor: "rgba(255,255,255,0)",
+                      color: "whitesmoke",
+                      width: "90%",
+                    }}
+                  >
+                    <Avatar
+                      key={haiku.user._id}
+                      className="totemPosition"
+                      sx={{ width: 70, height: 70 }}
+                      src={haiku.user.totem}
+                    />
+
+                    <Typography sx={{ marginTop: -5 }}>
+                      {haiku.line1}
+                    </Typography>
+                    <Typography>{haiku.line2}</Typography>
+                    <Typography>{haiku.line3}</Typography>
+                    <Avatar
+                      sx={{
+                        width: 30,
+                        height: 30,
+                        position: "relative",
+                        left: "100%",
+                      }}
+                      src={haiku.emoji}
+                    />
+                  </Paper>
+                </>
+              ) : null;
+            })}
+          </div>
+        </TabPanel>
+
+        <TabPanel value="2">My favoris</TabPanel>
+      </TabContext>
+    </Box>
+  );
 }
