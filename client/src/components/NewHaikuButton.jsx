0bd2f7ab8615ab64f01e3c4ea9958e1a344d489b
dmjahed <mjahedlucas.dorian@gmail.com>
2022-07-19 11:11:31 +0200

    [FIXED] Add NewHaikuButton - Need Responsive for larger screen

--- client/src/components/NewHaikuButton.jsx
+++ client/src/components/NewHaikuButton.jsx
@@ -1,12 +1,10 @@
 
 import React from "react";
-import Link from "@mui/material/Link"; 
 import NewHaikuButtonIcone from '../assets/icones/newHaikuButton.png'
 
 export default function NewHaikuButton() {
     return(
         <div className="newHaikuButton">
-        {/* Condition de connexion => connexion OK */}
         {localStorage.getItem("userId")
         ? 
             <a href="/newhaiku" >
@@ -17,7 +15,6 @@ export default function NewHaikuButton() {
                 <img src={NewHaikuButtonIcone} className="NewHaikuButtonIcone"></img>
             </a>
         }
-        {/* Condition de connexion => ERROR */}
         </div>
     );
 }
